46d175f039fb98b8e087a7008426c435d73d0828
Masayuki Ikeda <knnokpet@gmail.com>
2019-11-29 17:10:30 +0900

    Tweaks for FloatingViewController

--- FloatingViewController/FloatingViewController.swift
+++ FloatingViewController/FloatingViewController.swift
@@ -34,6 +34,10 @@ class FloatingViewController: UIViewController {
         self.view.layer.cornerRadius = 12
         self.view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         self.view.layer.masksToBounds = true
+        self.view.layer.borderColor = UIColor.black.withAlphaComponent(0.5).cgColor
+        self.view.layer.borderWidth = 0.2
+        
+        self.view.backgroundColor = UIColor.white.withAlphaComponent(0.1)
     }
     
     private func configureShadowView() {
@@ -47,9 +51,12 @@ class FloatingViewController: UIViewController {
 
     private func configureVisualEffectView() {
         self.visualEffectView.translatesAutoresizingMaskIntoConstraints = false
+        self.visualEffectView.layer.cornerRadius = 12
+        self.visualEffectView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        self.visualEffectView.layer.masksToBounds = true
         self.view.insertSubview(self.visualEffectView, at: 0)
         
-        self.visualEffectView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0.0).isActive = true
+        self.visualEffectView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0.2).isActive = true
         self.visualEffectView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0.0).isActive = true
         self.visualEffectView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0.0).isActive = true
         self.visualEffectView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0.0).isActive = true
