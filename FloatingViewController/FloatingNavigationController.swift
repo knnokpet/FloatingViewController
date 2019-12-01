e342b9f01df88c62b3b58c39aa89652b657f0c19
Masayuki Ikeda <knnokpet@gmail.com>
2019-12-01 20:37:24 +0900

    Fix a crash bug

--- FloatingViewController/FloatingNavigationController.swift
+++ FloatingViewController/FloatingNavigationController.swift
@@ -5,7 +5,19 @@ class FloatingNavigationController: UINavigationController, Floatable {
     let visualEffectView: UIVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight))
     
     let shadowView: CoverShadowView = CoverShadowView()
-
+    
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
     // MARK: - Load
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,16 +34,4 @@ class FloatingNavigationController: UINavigationController, Floatable {
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         NotificationCenter.default.post(name: .didChangeTraitCollection, object: self, userInfo: [FloatNotificationProperty.traitcollection: self.traitCollection as Any])
     }
-    
-    // MARK: - Configure
-    private func configureView() {
-        self.view.layer.cornerRadius = 12
-        self.view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        self.view.layer.masksToBounds = true
-        self.view.layer.borderColor = UIColor.black.withAlphaComponent(0.5).cgColor
-        self.view.layer.borderWidth = 0.2
-        
-        self.view.backgroundColor = UIColor.white.withAlphaComponent(0.1)
-    }
-
 }
